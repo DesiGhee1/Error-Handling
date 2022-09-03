aacb8d15cb25c42497c20f0044416b48b7628114
Hassaan Ahmed <ahassaan673@gmail.com>
2022-09-04 04:42:31 +0500

    test

---
+++ app.js
@@ -0,0 +1,14 @@
+// x*2+5y
+
+function math(x,y){
+    try{
+       
+       console.log((x**2)+5*y)
+       console.log("we can not catch an error")
+     }
+   catch(err){
+       console.log("this is error"+err)
+   }
+   }
+   math(3,5)
+   
